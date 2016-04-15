86cc07a67723c390d5a9c6716c758a36c181e0f3
Esteban Fuentealba <efuentealba@json.cl>
2016-04-14 22:25:19 -0300

    added ios feature

--- RNShare.ios.js
+++ RNShare.ios.js
@@ -4,7 +4,7 @@
  */
 'use strict';
 
-import {NativeModules} = from 'NativeModules';
+import {NativeModules} from 'react-native';
 let NativeRNShare = NativeModules.RNShare;
 /**
  * High-level docs for the RNShare iOS API can be written here.
