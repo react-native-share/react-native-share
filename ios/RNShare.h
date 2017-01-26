924501cc36c370f2e0f4aeaa3a86f331d00c4c2d
Enguerrand des Vaux <enguerrand.desvaux@gmail.com>
2017-01-26 17:18:17 +1000

    Update RNShare.h

--- ios/RNShare.h
+++ ios/RNShare.h
@@ -1,4 +1,12 @@
 @import UIKit;
+// import RCTBridgeModule
+#if __has_include(<React/RCTBridgeModule.h>)
+#import <React/RCTBridgeModule.h>
+#elif __has_include("RCTBridgeModule.h")
+#import "RCTBridgeModule.h"
+#else
+#import "React/RCTBridgeModule.h"   // Required when used as a Pod in a Swift project
+#endif
 #import <React/RCTBridgeModule.h>
 
 @interface RNShare : NSObject <RCTBridgeModule>
