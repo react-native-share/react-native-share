b0919b3d45cf4d6987d5161b5985d02259094fd1
Deep Mehta <deepmehta.it@live.com>
2017-06-05 20:29:39 -0700

    added null check for cursor and cursor.moveToFirst()
    Fix for NullPointerException on HTC M10 issue #131

--- android/src/main/java/cl/json/ShareFile.java
+++ android/src/main/java/cl/json/ShareFile.java
@@ -78,7 +78,11 @@ public class ShareFile {
             // try resolving the file and get the mimetype
             if(this.type == null) {
               String realPath = this.getRealPathFromURI(uri);
-              this.type = this.getMimeType(realPath);
+              if (realPath != null) {
+                  this.type = this.getMimeType(realPath);
+              } else {
+                  return false;
+              }
             }
 
             if(this.type == null) {
@@ -99,10 +103,12 @@ public class ShareFile {
         String[] proj = { MediaStore.Images.Media.DATA };
         CursorLoader loader = new CursorLoader(this.reactContext, contentUri, proj, null, null, null);
         Cursor cursor = loader.loadInBackground();
-        int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
-        cursor.moveToFirst();
-        String result = cursor.getString(column_index);
-        cursor.close();
+        String result = null;
+        if (cursor != null && cursor.moveToFirst()) {
+            int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+            result = cursor.getString(column_index);
+            cursor.close();
+        }
         return result;
     }
     public Uri getURI() {
