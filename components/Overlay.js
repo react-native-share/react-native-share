36316a9cd3ff4d24edfcdc62ba8afa21e0ef2a51
Edgar Aroutiounian <edgar.factorial@gmail.com>
2017-08-12 04:29:56 -0700

    Simplify JavaScript code in components

--- components/Overlay.js
+++ components/Overlay.js
@@ -1,55 +1,49 @@
 import React from 'react';
-import {
-  Animated,
-  StyleSheet,
-  View,
-  Dimensions,
-  TouchableHighlight
-} from 'react-native';
+import {Animated, StyleSheet, TouchableHighlight} from 'react-native';
 
 const DEFAULT_ANIMATE_TIME = 300;
 const styles = StyleSheet.create({
-    fullOverlay: {
-        top: 0,
-        bottom: 0,
-        left: 0,
-        right: 0,
-        backgroundColor: 'transparent',
-        position: 'absolute'
-    },
-    emptyOverlay: {
-        width: 0,
-        height: 0,
-        backgroundColor: 'transparent',
-        position: 'absolute'
-    }
-});
-export default class Overlay extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      fadeAnim: new Animated.Value(0),
-      overlayStyle: styles.emptyOverlay
-    }
+  fullOverlay: {
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
+    backgroundColor: 'transparent',
+    position: 'absolute'
+  },
+  emptyOverlay: {
+    width: 0,
+    height: 0,
+    backgroundColor: 'transparent',
+    position: 'absolute'
   }
+});
+
+export default class extends React.Component {
+  state = {
+    fadeAnim: new Animated.Value(0),
+    overlayStyle: styles.emptyOverlay
+  };
+
   onAnimatedEnd() {
-    if(!this.props.visible) {
-        this.setState({overlayStyle:styles.emptyOverlay});
+    if (!this.props.visible) {
+      this.setState({overlayStyle: styles.emptyOverlay});
     }
   }
   componentWillReceiveProps(newProps) {
-    if(newProps.visible){
+    if (newProps.visible) {
       this.setState({overlayStyle: styles.fullOverlay});
     }
     return Animated.timing(this.state.fadeAnim, {
-        toValue: newProps.visible ? 1 : 0,
-        duration: DEFAULT_ANIMATE_TIME
+      toValue: newProps.visible ? 1 : 0,
+      duration: DEFAULT_ANIMATE_TIME
     }).start(this.onAnimatedEnd.bind(this));
   }
   render() {
     return (
-      <Animated.View style={[this.state.overlayStyle, {opacity: this.state.fadeAnim}]}>
-          {this.props.children}
+      <Animated.View
+        style={[this.state.overlayStyle, {opacity: this.state.fadeAnim}]}>
+        {this.props.children}
       </Animated.View>
     );
   }
