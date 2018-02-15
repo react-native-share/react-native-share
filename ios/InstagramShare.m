6e44d01ed269c813bbf9c42c1257f908a8a4de2f
Ralf Nieuwenhuizen <ralfnieuwenhuizen@gmail.com>
2018-02-15 17:11:05 +0100

    Open instagram on camera if video to share is longer than 60 seconds

--- ios/InstagramShare.m
+++ ios/InstagramShare.m
@@ -6,6 +6,7 @@
 //
 
 #import "InstagramShare.h"
+#import <AVFoundation/AVFoundation.h>
 
 @implementation InstagramShare
 - (void)shareSingle:(NSDictionary *)options
@@ -13,34 +14,39 @@
     successCallback:(RCTResponseSenderBlock)successCallback {
     
     NSLog(@"Try open view");
-    
-    if ([options objectForKey:@"message"] && [options objectForKey:@"message"] != [NSNull null]) {
-        NSString *text = [RCTConvert NSString:options[@"message"]];
-        text = [text stringByAppendingString: [@" " stringByAppendingString: options[@"url"]] ];
-        text = (NSString*)CFBridgingRelease(CFURLCreateStringByAddingPercentEscapes(NULL,(CFStringRef) text, NULL,CFSTR("!*'();:@&=+$,/?%#[]"),kCFStringEncodingUTF8));
+
+    NSURL * fileURL = [NSURL URLWithString: options[@"url"]];
+    AVURLAsset* videoAsset = [AVURLAsset URLAssetWithURL:fileURL options:nil];
+    CMTime videoDuration = videoAsset.duration;
+    float videoDurationSeconds = CMTimeGetSeconds(videoDuration);
+
+    NSLog(@"Video duration: %f seconds for file %@", videoDurationSeconds, videoAsset.URL.absoluteString);
         
-        NSString * urlString = [NSString stringWithFormat:@"instagram://library?AssetPath=%@&InstagramCaption=%@", options[@"url"], text];
-        NSURL * shareURL = [NSURL URLWithString:urlString];
-        
-        if ([[UIApplication sharedApplication] canOpenURL: shareURL]) {
-            [[UIApplication sharedApplication] openURL: shareURL];
-            successCallback(@[]);
-        } else {
-            // Cannot open instagram
-            NSString *stringURL = @"http://itunes.apple.com/app/instagram/id389801252";
-            NSURL *url = [NSURL URLWithString:stringURL];
-            [[UIApplication sharedApplication] openURL:url];
-            
-            NSString *errorMessage = @"Not installed";
-            NSDictionary *userInfo = @{NSLocalizedFailureReasonErrorKey: NSLocalizedString(errorMessage, nil)};
-            NSError *error = [NSError errorWithDomain:@"com.rnshare" code:1 userInfo:userInfo];
-            
-            NSLog(errorMessage);
-            failureCallback(error);
-        }
+    NSURL * shareURL;
+    // Instagram doesn't allow sharing videos longer than 60 seconds on iOS anymore. (next button is not responding, trim is unavailable)
+    if (videoDurationSeconds <= 60.0f) {
+        NSString * urlString = [NSString stringWithFormat:@"instagram://library?AssetPath=%@", options[@"url"]];
+        shareURL = [NSURL URLWithString:urlString];
+    } else {
+        shareURL = [NSURL URLWithString:@"instagram://camera"];
     }
     
+    if ([[UIApplication sharedApplication] canOpenURL: shareURL]) {
+        [[UIApplication sharedApplication] openURL: shareURL];
+        successCallback(@[]);
+    } else {
+        // Cannot open instagram
+        NSString *stringURL = @"http://itunes.apple.com/app/instagram/id389801252";
+        NSURL *url = [NSURL URLWithString:stringURL];
+        [[UIApplication sharedApplication] openURL:url];
+        
+        NSString *errorMessage = @"Not installed";
+        NSDictionary *userInfo = @{NSLocalizedFailureReasonErrorKey: NSLocalizedString(errorMessage, nil)};
+        NSError *error = [NSError errorWithDomain:@"com.rnshare" code:1 userInfo:userInfo];
+        
+        NSLog(errorMessage);
+        failureCallback(error);
+    } 
 }
-
 
 @end
