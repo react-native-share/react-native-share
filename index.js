5d53ad61683f26c2228dcb8b2c941f9e8da84f6d
Sergey <gaykov@users.noreply.github.com>
2019-01-29 23:07:41 +0300

    Fix flow types for ShareSheet (#439)

--- index.js
+++ index.js
@@ -144,7 +144,7 @@ const requireAndAskPermissions = async (options: Options | MultipleOptions): Pro
 
 class RNShare {
   static Button: any;
-  static ShareSheet: React.createElement;
+  static ShareSheet: React.Element<*>;
   static Overlay: any;
   static Sheet: any;
   static Social = {
