312ade5e52093d15ecec0c4550c4a4864b3d8521
Bruno Ferreira <brunoferreira@Brunos-MacBook-Pro.local>
2019-02-26 21:12:16 -0300

    add video share stories

--- ios/InstagramStories.m
+++ ios/InstagramStories.m
@@ -82,17 +82,36 @@ backgroundBottomColor:(NSString *)backgroundBottomColor
     }
 }
 
+- (void)backgroundVideo:(NSData *)backgroundVideo
+{
+    // Verify app can open custom URL scheme. If able,
+    // assign assets to pasteboard, open scheme.
+    NSURL *urlScheme = [NSURL URLWithString:@"instagram-stories://share"];
+    if ([[UIApplication sharedApplication] canOpenURL:urlScheme]) {
+        // Assign background image asset and attribution link URL to pasteboard
+        NSArray *pasteboardItems = @[@{@"com.instagram.sharedSticker.backgroundVideo" : backgroundVideo}];
+        //NSArray *pasteboardItems = @[@{@"com.instagram.sharedSticker.backgroundVideo" : backgroundVideo;
+        NSDictionary *pasteboardOptions = @{UIPasteboardOptionExpirationDate : [[NSDate date] dateByAddingTimeInterval:60 * 5]};
+        // This call is iOS 10+, can use 'setItems' depending on what versions you support
+        [[UIPasteboard generalPasteboard] setItems:pasteboardItems options:pasteboardOptions]; [[UIApplication sharedApplication] openURL:urlScheme options:@{} completionHandler:nil];
+    } else { // Handle older app versions or app not installed case
+        [self fallbackInstagram];
+    }
+}
+
 
 - (void)shareSingle:(NSDictionary *)options
     failureCallback:(RCTResponseErrorBlock)failureCallback
     successCallback:(RCTResponseSenderBlock)successCallback {
     
     NSString *attrURL = [RCTConvert NSString:options[@"attributionURL"]];
+    //NSData *backgroundVideo = [[NSFileManager defaultManager] contentsAtPath:path];
     if (attrURL == nil) {
         attrURL = @"";
     }
     
     NSString *method = [RCTConvert NSString:options[@"method"]];
+       RCTLog(@"method method is");
     if (method) {
         if([method isEqualToString:@"shareBackgroundImage"]) {
             
@@ -137,6 +156,16 @@ backgroundBottomColor:(NSString *)backgroundBottomColor
                 UIImage *stickerImage = [UIImage imageWithData: [NSData dataWithContentsOfURL:sticketURL]];
                 
                 [self backgroundImage:UIImagePNGRepresentation(backgroundImage) stickerImage:UIImagePNGRepresentation(stickerImage) attributionURL:attrURL];
+            }
+        }else if([method isEqualToString:@"shareBackgroundVideo"]) {
+            
+            NSString *URL = [RCTConvert NSString:options[@"backgroundVideo"]];
+            if (URL == nil) {
+                RCTLogError(@"key 'backgroundVideo' missing in options");
+            } else {
+                NSData *backgroundVideo = [[NSFileManager defaultManager] contentsAtPath: URL];
+                
+                [self backgroundVideo: backgroundVideo];
             }
         }
     } else {
