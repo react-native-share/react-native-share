00f57bf08bbf5172726e4e8d397019537f8a5132
Vincent Guerandel <guerandelv@redcat.com.au>
2019-03-01 11:51:47 +1100

    Set social and InstagramStories as empty dicts for ios

--- index.js
+++ index.js
@@ -147,24 +147,29 @@ class RNShare {
   static ShareSheet: React.Element<*>;
   static Overlay: any;
   static Sheet: any;
-  static Social = {
-    FACEBOOK: NativeModules.RNShare.FACEBOOK || 'facebook',
-    PAGESMANAGER: NativeModules.RNShare.PAGESMANAGER || 'pagesmanager',
-    TWITTER: NativeModules.RNShare.TWITTER || 'twitter',
-    WHATSAPP: NativeModules.RNShare.WHATSAPP || 'whatsapp',
-    INSTAGRAM: NativeModules.RNShare.INSTAGRAM || 'instagram',
-    INSTAGRAM_STORIES: NativeModules.RNShare.INSTAGRAM_STORIES || 'instagram-stories',
-    GOOGLEPLUS: NativeModules.RNShare.GOOGLEPLUS || 'googleplus',
-    EMAIL: NativeModules.RNShare.EMAIL || 'email',
-    PINTEREST: NativeModules.RNShare.PINTEREST || 'pinterest',
-    SMS: NativeModules.RNShare.SMS || 'sms',
-  };
 
-  static InstagramStories = {
+  static Social = (Platform.OS === 'android')
+    ? {
+      FACEBOOK: NativeModules.RNShare.FACEBOOK || 'facebook',
+      PAGESMANAGER: NativeModules.RNShare.PAGESMANAGER || 'pagesmanager',
+      TWITTER: NativeModules.RNShare.TWITTER || 'twitter',
+      WHATSAPP: NativeModules.RNShare.WHATSAPP || 'whatsapp',
+      INSTAGRAM: NativeModules.RNShare.INSTAGRAM || 'instagram',
+      INSTAGRAM_STORIES: NativeModules.RNShare.INSTAGRAM_STORIES || 'instagram-stories',
+      GOOGLEPLUS: NativeModules.RNShare.GOOGLEPLUS || 'googleplus',
+      EMAIL: NativeModules.RNShare.EMAIL || 'email',
+      PINTEREST: NativeModules.RNShare.PINTEREST || 'pinterest',
+      SMS: NativeModules.RNShare.SMS || 'sms',
+  }
+  : {};
+
+  static InstagramStories = (Platform.OS === 'android')
+  ? {
     SHARE_BACKGROUND_IMAGE: NativeModules.RNShare.SHARE_BACKGROUND_IMAGE || 'shareBackgroundImage',
     SHARE_STICKER_IMAGE: NativeModules.RNShare.SHARE_STICKER_IMAGE || 'shareStickerImage',
     SHARE_BACKGROUND_AND_STICKER_IMAGE: NativeModules.RNShare.SHARE_BACKGROUND_AND_STICKER_IMAGE || 'shareBackgroundAndStickerImage',
   }
+  : {};
 
   static open(options: Options | MultipleOptions): Promise<OpenReturn> {
     return new Promise((resolve, reject) => {
