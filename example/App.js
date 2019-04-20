da092271e648923526de49f6c332d47ff0fff37e
MateusAndrade <mateus.andrade47@outlook.com>
2019-04-20 12:30:34 -0300

    Removing unused instruction on App.js

--- example/App.js
+++ example/App.js
@@ -5,23 +5,17 @@
  */
 
 import React, { Component } from 'react';
+
 import {
-  Platform,
   StyleSheet,
   Text,
   View,
   Button,
-  Alert
+  Alert,
 } from 'react-native';
 import Share from 'react-native-share';
-import images from './src/imageBase64';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+import images from './src/imageBase64';
 
 type Props = {};
 export default class App extends Component<Props> {
@@ -76,10 +70,5 @@ const styles = StyleSheet.create({
     fontSize: 20,
     textAlign: 'center',
     margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
   },
 });
