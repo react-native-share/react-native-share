804ad4301900b3e0ed3403391ca6c0c77f02608d
MateusAndrade <mateus.andrade47@outlook.com>
2019-04-20 00:35:13 -0300

    Changing sharesheet type to any since there is a flow error on module.exports

--- index.js
+++ index.js
@@ -144,7 +144,7 @@ const requireAndAskPermissions = async (options: Options | MultipleOptions): Pro
 
 class RNShare {
   static Button: any;
-  static ShareSheet: React.Element<*>;
+  static ShareSheet: any;
   static Overlay: any;
   static Sheet: any;
   static Social = {
