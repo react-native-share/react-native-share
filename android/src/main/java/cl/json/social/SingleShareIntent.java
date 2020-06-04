87cdecf9b8274ab346aeda4386b904ac84c66fae
Vladimir Stalmakov <stal.the.skier@gmail.com>
2020-06-04 15:07:29 +0300

    fix: added additionlat attribute check

--- android/src/main/java/cl/json/social/SingleShareIntent.java
+++ android/src/main/java/cl/json/social/SingleShareIntent.java
@@ -69,13 +69,21 @@ public abstract class SingleShareIntent extends ShareIntent {
                 TargetChosenReceiver.sendCallback(false, "Something went wrong");
                 return;
             }
-            if (options != null && !options.isEmpty()) {
-                String socialType = options.getString("social");
-                if (socialType.equals("instagramstories")) {
-                    String method = options.getString("method");
-                    if (method.equals("shareStickerImage") || method.equals("shareBackgroundAndStickerImage")) {
-                        activity.grantUriPermission("com.instagram.android", this.stickerAsset.getURI(), Intent.FLAG_GRANT_READ_URI_PERMISSION);
+            if (options != null) {
+                if (ShareIntent.hasValidKey("social", options)) {
+                    String socialType = options.getString("social");
+                    if (socialType.equals("instagramstories")) {
+                        if (ShareIntent.hasValidKey("method", options)) {
+                            String method = options.getString("method");
+                            if (method.equals("shareStickerImage") || method.equals("shareBackgroundAndStickerImage")) {
+                                activity.grantUriPermission("com.instagram.android", this.stickerAsset.getURI(), Intent.FLAG_GRANT_READ_URI_PERMISSION);
+                            }
+                        } else {
+                            throw new java.lang.IllegalArgumentException("instagram share mode is empty");
+                        }
                     }
+                } else {
+                    throw new java.lang.IllegalArgumentException("social is empty");
                 }
             }
             if (TargetChosenReceiver.isSupported()) {
