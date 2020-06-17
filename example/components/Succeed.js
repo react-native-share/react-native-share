e169d6692e22a66e5c732c43d8d07be5f16a1e5e
Mateus Andrade <mateus.andrade47@outlook.com>
2020-06-17 11:30:51 -0300

    chore: Merge pull request #803 from MateusAndrade/example-local-version
    * chore: updating example to user root rn-share-version
    
    * chore: changing index.js location
    
    * chore: changing example bundle location
    
    * chore: removing dev-sync script
    
    * ci: fixing Example location
    
    * fix: adding downloadDependencies task
    
    * ci: changing project path
    
    * ci: adding cd to Example folder
    
    * ci: adding cd to Example folder
    
    * ci: adding cd with project
    
    * ci: removing attach to workspace
    
    * ci: changing attach location
    
    * ci: adding ls -la
    
    * ci: using build command
    
    * ci: using build command
    
    * ci: testing cd script
    
    * ci: testing build
    
    * ci: testing build
    
    * ci: testing build
    
    * ci: testing build
    
    * ci: testing build
    
    * ci: testing build
    
    * ci: testing build
    
    * ci: testing build
    
    * ci: testing build
    
    * ci: testing build
    
    * ci: testing build
    
    * ci: testing build
    
    * ci: testing build
    
    * ci: testing build
    
    * fix: adding LSApplicationQueriesSchemes to example
    
    * ci: adding ios build command
    
    * fix: scheme on ci
    
    * fix: adding standalone job due to error with orb
    
    * ci: changing ios build device to 11
    
    * fix: disableing build sign on xcode
    
    * fix: adding pod install
    
    * fix: updating Flipper to 0.37 on example due to a crash with base64
    
    * docs: adding proper info about example app
    
    * docs: adding explanation about documentation
    
    * docs: adding SEO to home page

--- example/components/Succeed.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export default class Succeed extends React.Component {
-  render() {
-    return null;
-  }
-}
--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import classnames from 'classnames';
 import Layout from '@theme/Layout';
 import Link from '@docusaurus/Link';
+import Head from '@docusaurus/Head';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import useBaseUrl from '@docusaurus/useBaseUrl';
 import styles from './styles.module.css';
@@ -10,33 +11,23 @@ const features = [
   {
     title: <>Easy to Use</>,
     imageUrl: 'img/app_data.svg',
-    description: (
-      <>
-        Easily share data between applications.
-      </>
-    ),
+    description: <>Easily share data between applications.</>,
   },
   {
     title: <>Share files</>,
     imageUrl: 'img/attached_file.svg',
     description: (
-      <>
-        Share files using base64 or the file url on the device(according to each platform)  
-      </>
+      <>Share files using base64 or the file url on the device(according to each platform)</>
     ),
   },
   {
     title: <>Powered by react-native-community</>,
     imageUrl: 'img/react.svg',
-    description: (
-      <>
-        Help us out with issues, PR's and many more: https://bit.ly/3cI7xNb.
-      </>
-    ),
+    description: <>Help us out with issues, PR's and many more: https://bit.ly/3cI7xNb.</>,
   },
 ];
 
-function Feature({imageUrl, title, description}) {
+function Feature({ imageUrl, title, description }) {
   const imgUrl = useBaseUrl(imageUrl);
   return (
     <div className={classnames('col col--4', styles.feature)}>
@@ -53,41 +44,48 @@ function Feature({imageUrl, title, description}) {
 
 function Home() {
   const context = useDocusaurusContext();
-  const {siteConfig = {}} = context;
+  const { siteConfig = {} } = context;
   return (
-    <Layout
-      title={`Docs of ${siteConfig.projectName} 📤`}
-      description={siteConfig.tagline}>
-      <header className={classnames('hero hero--primary', styles.heroBanner)}>
-        <div className="container">
-          <h1 className="hero__title">{siteConfig.title}</h1>
-          <p className="hero__subtitle">{siteConfig.tagline}</p>
-          <div className={styles.buttons}>
-            <Link
-              className={classnames(
-                'button button--outline button--secondary button--lg',
-                styles.getStarted,
-              )}
-              to={useBaseUrl('docs/')}>
-              Get Started
-            </Link>
-          </div>
-        </div>
-      </header>
-      <main>
-        {features && features.length > 0 && (
-          <section className={styles.features}>
-            <div className="container">
-              <div className="row">
-                {features.map((props, idx) => (
-                  <Feature key={idx} {...props} />
-                ))}
-              </div>
+    <>
+      <Head>
+        <meta property="og:description" content="react-native-share" />
+        <meta charSet="utf-8" />
+        <title>react-native-share</title>
+        <link rel="canonical" href="https://react-native-community.github.io/react-native-share/" />
+      </Head>
+      <Layout title={`Docs of ${siteConfig.projectName} 📤`} description={siteConfig.tagline}>
+        <header className={classnames('hero hero--primary', styles.heroBanner)}>
+          <div className="container">
+            <h1 className="hero__title">{siteConfig.title}</h1>
+            <p className="hero__subtitle">{siteConfig.tagline}</p>
+            <div className={styles.buttons}>
+              <Link
+                className={classnames(
+                  'button button--outline button--secondary button--lg',
+                  styles.getStarted,
+                )}
+                to={useBaseUrl('docs/')}
+              >
+                Get Started
+              </Link>
             </div>
-          </section>
-        )}
-      </main>
-    </Layout>
+          </div>
+        </header>
+        <main>
+          {features && features.length > 0 && (
+            <section className={styles.features}>
+              <div className="container">
+                <div className="row">
+                  {features.map((props, idx) => (
+                    <Feature key={idx} {...props} />
+                  ))}
+                </div>
+              </div>
+            </section>
+          )}
+        </main>
+      </Layout>
+    </>
   );
 }
 
