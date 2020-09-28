d7ca23afd784fd44871a5dd4d1e8a412e37a9682
Wes Johnson <wjohnson@wealthsimple.com>
2020-09-28 13:47:35 -0400

    docs: update docs for FileProvider, permission prompt & write location changes

--- website/sidebars.js
+++ website/sidebars.js
@@ -1,7 +1,16 @@
 module.exports = {
   someSidebar: {
-    'Getting Started': ['install', "share-open" ,"share-single" ,"share-is-package-installed", "ui-components", "language-support","testing"],
-    'Recipes' : ['share-remote-file'],
-    'Troubleshooting': ["troubleshooting-ios"],
+    'Getting Started': [
+      'install',
+      'share-open',
+      'share-single',
+      'share-is-package-installed',
+      'ui-components',
+      'language-support',
+      'testing',
+    ],
+    Recipes: ['share-remote-file'],
+    Migrating: ['migrate-v3-to-v4'],
+    Troubleshooting: ['troubleshooting-ios'],
   },
 };
