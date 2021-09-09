c808116fd16957deba85bbfa40e5034ff7b28afb
Dawid Dereszewski <ddereszewski@gmail.com>
2021-09-09 09:03:11 +0200

    fix: pr comments

--- android/src/main/java/cl/json/social/InstagramShare.java
+++ android/src/main/java/cl/json/social/InstagramShare.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import java.io.File;
 import android.os.Environment;
 import android.net.Uri;
+import android.util.Log;
 
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReadableMap;
@@ -25,13 +26,13 @@ public class InstagramShare extends SingleShareIntent {
     public void open(ReadableMap options) throws ActivityNotFoundException {
         super.open(options);
         try {
-            // passing instagram://share as url option opens Camera View for publishing
+            // passing instagram://share as url option opens Publishing screen with camera view
             if (ShareIntent.hasValidKey("url", options) && options.getString("url").startsWith("instagram://")) {
                 this.getIntent().setAction(Intent.ACTION_VIEW);
                 this.getIntent().setData(Uri.parse(options.getString("url")));
             }
-        }catch (Exception e){
-            // bad param passed
+        } catch (Exception e) {
+            Log.w("RNShare", e.getMessage());
         }
         this.openIntentChooser();
     }
