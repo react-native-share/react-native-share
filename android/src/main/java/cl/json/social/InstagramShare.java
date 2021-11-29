4008b5d846bc993d20f66b55aa4682b771cbfc80
Alexis Rougnant <alexis.rougnant@gmail.com>
2021-11-29 17:59:29 +0100

    fix(instagram): fix black screen issue for instagram stories with Social.Instagram (#1126)
    * fix(instagram): open custom intent chooser for image data that supports instagram stories
    
    * fix(instagram): fix merge conflict
    
    * fix(instagram): prevent returning if type is not specified ffor the url scheme case
    
    * docs(singleShare): reorganize sections and add ishare image to instagram section
    
    * test: trigger circle ci

--- android/src/main/java/cl/json/social/InstagramShare.java
+++ android/src/main/java/cl/json/social/InstagramShare.java
@@ -1,14 +1,15 @@
 package cl.json.social;
 
+import android.app.Activity;
 import android.content.ActivityNotFoundException;
 import android.content.Intent;
-import java.io.File;
-import android.os.Environment;
 import android.net.Uri;
 import android.util.Log;
 
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReadableMap;
+
+import cl.json.ShareFile;
 
 /**
  * Created by Ralf Nieuwenhuizen on 10-04-17.
@@ -24,17 +25,64 @@ public class InstagramShare extends SingleShareIntent {
 
     @Override
     public void open(ReadableMap options) throws ActivityNotFoundException {
-        super.open(options);
-        try {
-            // passing instagram://share as url option opens Publishing screen with camera view
-            if (ShareIntent.hasValidKey("url", options) && options.getString("url").startsWith("instagram://")) {
-                this.getIntent().setAction(Intent.ACTION_VIEW);
-                this.getIntent().setData(Uri.parse(options.getString("url")));
+            super.open(options);
+
+            if (!ShareIntent.hasValidKey("url", options)) {
+                Log.e("RNShare", "No url provided");
+                return;
             }
-        } catch (Exception e) {
-            Log.w("RNShare", e.getMessage());
-        }
-        this.openIntentChooser();
+            String url = options.getString("url");
+
+            Boolean isUrlScheme = url.startsWith("instagram://");
+            if (isUrlScheme) {
+                openInstagramUrlScheme(url);
+                return;
+            }
+
+
+            if (!ShareIntent.hasValidKey("type", options)) {
+                Log.e("RNShare", "No type provided");
+                return;
+            }
+            String type = options.getString("type");
+            Boolean isImage = type.startsWith("image");
+
+            if (isImage) {
+                this.openInstagramIntentChooserForImage(url, chooserTitle);
+            } else {
+                super.openIntentChooser();
+            }
+    }
+
+    protected void openInstagramUrlScheme(String url) {
+            Uri uri = Uri.parse(url);
+            this.getIntent().setAction(Intent.ACTION_VIEW);
+            this.getIntent().setData(uri);
+            super.openIntentChooser();
+    }
+
+    protected void openInstagramIntentChooserForImage(String url, String chooserTitle) {
+        Boolean shouldUseInternalStorage = ShareIntent.hasValidKey("useInternalStorage", options) && options.getBoolean("useInternalStorage");
+        ShareFile shareFile = new ShareFile(url, "image/jpeg", "image", shouldUseInternalStorage, this.reactContext);
+        Uri uri = shareFile.getURI();
+
+        Intent feedIntent = new Intent(Intent.ACTION_SEND);
+        feedIntent.setType("image/*");
+        feedIntent.putExtra(Intent.EXTRA_STREAM, uri);
+        feedIntent.setPackage(PACKAGE);
+
+        Intent storiesIntent = new Intent("com.instagram.share.ADD_TO_STORY");
+        storiesIntent.setDataAndType(uri, "jpg");
+        storiesIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+        storiesIntent.setPackage(PACKAGE);
+
+        Intent chooserIntent = Intent.createChooser(feedIntent, chooserTitle);
+        chooserIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, new Intent[] {storiesIntent});
+
+        Activity activity = this.reactContext.getCurrentActivity();
+        activity.grantUriPermission(PACKAGE, uri, Intent.FLAG_GRANT_READ_URI_PERMISSION);
+        this.reactContext.startActivity(chooserIntent);
     }
 
     @Override
