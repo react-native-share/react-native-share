54a80d0c259657389e1b593cffc5262fe0b0e4b8
Hezron-Nelapati <62951600+Hezron-Nelapati@users.noreply.github.com>
2022-04-15 09:23:39 +0530

    fix(ios): custom title got over written by meta data (#1184)
    * Removed overriding of the title from meta data
    
    * Fix: Custom Title Overridden by Metadata

--- ios/RNShareActivityItemSource.m
+++ ios/RNShareActivityItemSource.m
@@ -66,7 +66,9 @@
             } else {
                 self->linkMetadata.originalURL = metadata.originalURL;
                 self->linkMetadata.URL = metadata.URL;
-                self->linkMetadata.title = metadata.title;
+                if(!self->linkMetadata.title) {
+                    self->linkMetadata.title = metadata.title;
+                }
                 self->linkMetadata.imageProvider = metadata.imageProvider;
                 if (self->linkMetadata.imageProvider) {
                     self->linkMetadata.iconProvider = self->linkMetadata.imageProvider;
