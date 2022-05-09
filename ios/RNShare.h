96e93ce487a51b60391b8de7551ec11655e6ebbe
Tyler Scott Williams <tyler@ogdenstudios.xyz>
2022-05-09 06:54:17 -0600

    fix(ios): remove old style double-quote imports (#1198)
    Co-authored-by: Tyler Williams <tywilliams@gitlab.com>

--- ios/RNShare.h
+++ ios/RNShare.h
@@ -1,12 +1,6 @@
 @import UIKit;
 // import RCTBridgeModule
-#if __has_include(<React/RCTBridgeModule.h>)
 #import <React/RCTBridgeModule.h>
-#elif __has_include("RCTBridgeModule.h")
-#import "RCTBridgeModule.h"
-#else
-#import "React/RCTBridgeModule.h"   // Required when used as a Pod in a Swift project
-#endif
 
 @interface RNShare : NSObject <RCTBridgeModule, UIDocumentPickerDelegate>
 
--- ios/RNShareActivityItemSource.m
+++ ios/RNShareActivityItemSource.m
@@ -5,22 +5,10 @@
 #endif
 
 // import RCTBridgeModule
-#if __has_include(<React/RCTBridgeModule.h>)
 #import <React/RCTBridgeModule.h>
-#elif __has_include("RCTBridgeModule.h")
-#import "RCTBridgeModule.h"
-#else
-#import "React/RCTBridgeModule.h"   // Required when used as a Pod in a Swift project
-#endif
 
 // import RCTConvert
-#if __has_include(<React/RCTConvert.h>)
 #import <React/RCTConvert.h>
-#elif __has_include("RCTConvert.h")
-#import "RCTConvert.h"
-#else
-#import "React/RCTConvert.h"   // Required when used as a Pod in a Swift project
-#endif
 
 @implementation RNShareActivityItemSource {
     id placeholderItem;
