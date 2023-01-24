53e32537db55900bdd69bd492ca03e100a7ad217
Max Arreaga <34686490+Maxarreaga@users.noreply.github.com>
2023-01-24 01:32:44 -0800

    fixing snapchat on android 13

--- android/src/main/java/cl/json/social/SnapChatShare.java
+++ android/src/main/java/cl/json/social/SnapChatShare.java
@@ -15,7 +15,7 @@ import com.facebook.react.bridge.ReadableMap;
 public class SnapChatShare extends SingleShareIntent {
 
     private static final String PACKAGE = "com.snapchat.android";
-    private static final String CLASS = "com.snapchat.android.LandingPageActivity";
+    private static final String CLASS = "com.snap.mushroom.MainActivity";
     private static final String PLAY_STORE_LINK = "market://details?id=com.snapchat.android";
     
     public SnapChatShare(ReactApplicationContext reactContext) {
