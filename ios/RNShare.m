4dbf8711e31c55e05028ca39aa600ab40343b0da
Linh <vovanlinh29546@gmail.com>
2023-05-29 09:30:43 +0700

    [IOS] support `filenames` to share

--- ios/RNShare.m
+++ ios/RNShare.m
@@ -179,7 +179,7 @@ RCT_EXPORT_METHOD(open:(NSDictionary *)options
     BOOL saveToFiles = [RCTConvert BOOL:options[@"saveToFiles"]];
     NSString *filename = [RCTConvert NSString:options[@"filename"]];
 
-    NSArray *filenames = options[@"filenames"];
+    NSArray *filenamesArray = options[@"filenames"];
     NSArray *urlsArray = options[@"urls"];
     
     for (int i=0; i<urlsArray.count; i++) {
@@ -194,19 +194,20 @@ RCT_EXPORT_METHOD(open:(NSDictionary *)options
                     failureCallback(error);
                     return;
                 }
-                if (saveToFiles) {
-
-                    //get filename and replace by index
-                     @try {
-                        NSString *fileNameByIndex = [RCTConvert NSString:filenames[i]];
-                        if(fileNameByIndex.length>0){
-                            //replace filename with name get by index
-                            filename=fileNameByIndex;
-                        }
+                
+                //name get from array of filenames
+                @try {
+                    NSString *fileNameByIndex = [RCTConvert NSString:filenamesArray[i]];
+                    if(fileNameByIndex.length>0){
+                        //replace filename with name get by index
+                        filename=fileNameByIndex;
                     }
-                    @catch (NSException * e) {
+                }
+                @catch (NSException * e) {
                         RCTLogError(@"Exception get filename from finames array: %@", e);
-                    }
+                }
+
+                if (saveToFiles) {
 
                     NSURL *filePath = [RNShareUtils getPathFromBase64:URL.absoluteString with:data fileName:filename];
 
