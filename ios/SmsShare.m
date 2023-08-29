7addec93ad998a6db1ac04a384a631998870081f
Andrei Calazans <andreixoc@hotmail.com>
2023-08-29 13:58:31 -0300

    feat(ios): extend sms share to support url

--- ios/SmsShare.m
+++ ios/SmsShare.m
@@ -31,6 +31,36 @@
         }
         mc.recipients = recipients;
         mc.body = message;
+        
+        NSURL *URL = [RCTConvert NSURL:options[@"url"]];
+        if (URL) {
+            BOOL isDataScheme = [URL.scheme.lowercaseString isEqualToString:@"data"];
+    
+            // Only handling data scheme urls here. To handle the case of URL.isFileURL
+            // one could add a case similar to the process in EmailShare.m
+            if (isDataScheme) {
+                NSError *error;
+                NSData *data = [NSData dataWithContentsOfURL:URL
+                                                     options:(NSDataReadingOptions)0
+                                                       error:&error];
+                if (!data) {
+                    reject(@"com.rnshare", @"No data", error);
+                    return;
+                }
+    
+                NSURL *filePath = [RNShareUtils getPathFromBase64:URL.absoluteString with:data fileName:@"file"];
+                if (filePath) {
+                    // public.image typeIdentifier works for both images and files
+                    [mc addAttachmentData:data typeIdentifier:@"public.image" filename:filePath.absoluteString];
+                }
+            } else {
+                // if not a file, just append URL to message
+                NSString *urlString = [RCTConvert NSString:options[@"url"]];
+                message = [message stringByAppendingString: [@" " stringByAppendingString:  urlString]];
+                [mc setBody:message];
+            }
+        }
+
 
         dispatch_async(dispatch_get_main_queue(), ^{
             UIViewController *ctrl = RCTPresentedViewController();
