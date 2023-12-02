a2b085eb5b50763867264ea12fea7cd23bcc72fd
zhenkaGo <zhenka.go@gmail.com>
2023-12-02 17:27:07 +0200

    fix(ios): Build fail in New Architecture (#1490)

--- ios/RNShare.h
+++ ios/RNShare.h
@@ -9,7 +9,7 @@
 #endif
 
 #if RCT_NEW_ARCH_ENABLED
-#import <React-Codegen/RNShareSpec/RNShareSpec.h>
+#import <RNShareSpec/RNShareSpec.h>
 #endif
 
 @interface RNShare : NSObject <RCTBridgeModule, UIDocumentPickerDelegate>
