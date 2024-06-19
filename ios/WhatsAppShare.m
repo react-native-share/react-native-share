df44b22184217c74c4932fb2d2337b47e1f84cb3
Lucas Garcez <lucasgarcezdev@gmail.com>
2024-06-20 01:01:55 +1000

    chore(ios): Improve WhatsAppShare readability (#1542)
    * refactor: improve WhatsAppShare readability
    
    * refactor: improve WhatsAppShare readability

--- ios/WhatsAppShare.m
+++ ios/WhatsAppShare.m
@@ -8,87 +8,169 @@
 
 #import "WhatsAppShare.h"
 
+typedef NS_ENUM(NSInteger, MessageType) {
+  MessageTypeImage,
+  MessageTypeVideo,
+  MessageTypeText
+};
+
 @implementation WhatsAppShare
 static UIDocumentInteractionController *documentInteractionController;
 RCT_EXPORT_MODULE();
-- (void)shareSingle:(NSDictionary *)options
-    reject:(RCTPromiseRejectBlock)reject
-    resolve:(RCTPromiseResolveBlock)resolve {
+
+
+- (void) shareSingle:(NSDictionary *)options
+reject:(RCTPromiseRejectBlock)reject
+resolve:(RCTPromiseResolveBlock)resolve {
     
     NSLog(@"Try open view");
     
-    if ([options objectForKey:@"message"] && [options objectForKey:@"message"] != [NSNull null]) {
-        NSString *text = [RCTConvert NSString:options[@"message"]];
-        text = [text stringByAppendingString: [@" " stringByAppendingString: options[@"url"]] ];
-        NSString *whatsAppNumber = [RCTConvert NSString:options[@"whatsAppNumber"]];
-        
-        if ([[UIApplication sharedApplication] canOpenURL: [NSURL URLWithString:@"whatsapp://app"]]) {
-            NSLog(@"WhatsApp installed");
-        } else {
-            // Cannot open whatsapp
-            NSString *stringURL = @"https://itunes.apple.com/app/whatsapp-messenger/id310633997";
-            NSURL *url = [NSURL URLWithString:stringURL];
-            [[UIApplication sharedApplication] openURL:url];
-            
-            NSString *errorMessage = @"Not installed";
-            NSDictionary *userInfo = @{NSLocalizedFailureReasonErrorKey: NSLocalizedString(errorMessage, nil)};
-            NSError *error = [NSError errorWithDomain:@"com.rnshare" code:1 userInfo:userInfo];
-            
-            NSLog(@"%@", errorMessage);
-            return reject(@"com.rnshare",@"Not installed",error);
-        }
-        
-        if ([options[@"url"] rangeOfString:@".wam"].location != NSNotFound || [options[@"url"] rangeOfString:@".mp4"].location != NSNotFound) {
-            NSLog(@"Sending whatsapp movie");
-            documentInteractionController = [UIDocumentInteractionController interactionControllerWithURL:[NSURL fileURLWithPath:options[@"url"]]];
-            documentInteractionController.UTI = @"net.whatsapp.movie";
-            documentInteractionController.delegate = self;
-            
-            [documentInteractionController presentOpenInMenuFromRect:CGRectZero inView:[[[[[UIApplication sharedApplication] delegate] window] rootViewController] view] animated:YES];
-            NSLog(@"Done whatsapp movie");
-            resolve(@[@true, @""]);
-        } else if ([options[@"url"] rangeOfString:@"png"].location != NSNotFound || [options[@"url"] rangeOfString:@"jpeg"].location != NSNotFound || [options[@"url"] rangeOfString:@"jpg"].location != NSNotFound || [options[@"url"] rangeOfString:@"gif"].location != NSNotFound) {
-            UIImage * image;
-            NSURL *imageURL = [RCTConvert NSURL:options[@"url"]];
-            if (imageURL) {
-                if (imageURL.fileURL || [imageURL.scheme.lowercaseString isEqualToString:@"data"]) {
-                    NSError *error;
-                    NSData *data = [NSData dataWithContentsOfURL:imageURL
-                                                         options:(NSDataReadingOptions)0
-                                                           error:&error];
-                    if (!data) {
-                        NSError *error = [NSError errorWithDomain:@"com.rnshare" code:2 userInfo:@{ NSLocalizedDescriptionKey:@"Something went wrong"}];
-                        return reject(@"com.rnshare",@"Something went wrong",error);
-                    }
-                    image = [UIImage imageWithData: data];
-                    NSString * documentsPath = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) objectAtIndex:0];
-                    NSString * filePath = [documentsPath stringByAppendingPathComponent:@"/whatsAppTmp.wai"];
-                    
-                    [UIImageJPEGRepresentation(image, 1.0) writeToFile:filePath atomically:YES];
-                    
-                    documentInteractionController = [UIDocumentInteractionController interactionControllerWithURL:[NSURL fileURLWithPath:filePath]];
-                    documentInteractionController.UTI = @"net.whatsapp.image";
-                    documentInteractionController.delegate = self;
-                    
-                    [documentInteractionController presentOpenInMenuFromRect:CGRectZero inView:[[[[[UIApplication sharedApplication] delegate] window] rootViewController] view] animated:YES];
-                    resolve(@[@true, @""]);
-                }
-            } else {
-                NSError *error = [NSError errorWithDomain:@"com.rnshare" code:3 userInfo:@{ NSLocalizedDescriptionKey:@"Something went wrong"}];
-                return reject(@"com.rnshare",@"Something went wrong",error);
-            }
-        } else {
-            text = (NSString*)CFBridgingRelease(CFURLCreateStringByAddingPercentEscapes(NULL,(CFStringRef) text, NULL,CFSTR("!*'();:@&=+$,/?%#[]"),kCFStringEncodingUTF8));
-            
-            NSString * urlWhats = whatsAppNumber ? [NSString stringWithFormat:@"whatsapp://send?phone=%@&text=%@", whatsAppNumber, text] : [NSString stringWithFormat:@"whatsapp://send?text=%@", text];
-            NSURL * whatsappURL = [NSURL URLWithString:urlWhats];
-            
-            if ([[UIApplication sharedApplication] canOpenURL: whatsappURL]) {
-                [[UIApplication sharedApplication] openURL: whatsappURL];
-                resolve(@[@true, @""]);
-            }
-        }
+    if(![self isAbleToSendMessage: options]){
+        return;
     }
+    
+    if(![self isWhatsAppAvailable]) {
+        [self handleError:@"Not Installed" code:1 rejectFn:reject];
+        return;
+    }
+    
+    MessageType messageType = [self getMessageType: options[@"url"]];
+    
+    switch(messageType) {
+      case MessageTypeImage:
+        [self tryToSendImage: options resolve:resolve reject:reject];
+        break;
+        
+      case MessageTypeVideo:
+        [self tryToSendVideo: options resolve:resolve reject:reject];
+        break;
+        
+      default:
+        [self tryToSendText: options resolve:resolve reject:reject];
+    }
+    
+}
+
+-(void)tryToSendImage:(NSDictionary *)options
+              resolve:(RCTPromiseResolveBlock)resolve
+              reject:(RCTPromiseRejectBlock)reject {
+  UIImage *image;
+  NSURL *imageURL = [RCTConvert NSURL:options[@"url"]];
+  if(imageURL){
+    if (imageURL.fileURL || [imageURL.scheme.lowercaseString isEqualToString:@"data"]) {
+        NSError *error;
+        NSData *data = [NSData dataWithContentsOfURL:imageURL
+                                             options:(NSDataReadingOptions)0
+                                               error:&error];
+        if (!data) {
+          return [self handleError:@"Something went wrong" code:2 rejectFn:reject];
+        }
+      
+        image = [UIImage imageWithData: data];
+        NSString * documentsPath = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) objectAtIndex:0];
+        NSString * filePath = [documentsPath stringByAppendingPathComponent:@"/whatsAppTmp.wai"];
+        
+        [UIImageJPEGRepresentation(image, 1.0) writeToFile:filePath atomically:YES];
+      
+        [self shareMedia:filePath documentUTI:@"net.whatsapp.image"];
+      
+        resolve(@[@true, @""]);
+    }
+
+  }else {
+    [self handleError:@"Something went wrong" code:3 rejectFn:reject];
+  }
+  
+}
+
+-(void)tryToSendVideo:(NSDictionary *)options
+              resolve:(RCTPromiseResolveBlock)resolve
+              reject:(RCTPromiseRejectBlock)reject {
+  
+  NSLog(@"Sending whatsapp movie");
+  [self shareMedia:options[@"url"] documentUTI:@"net.whatsapp.movie"];
+  NSLog(@"Done whatsapp movie");
+  resolve(@[@true, @""]);
+}
+
+-(void)tryToSendText:(NSDictionary *)options
+             resolve:(RCTPromiseResolveBlock)resolve
+             reject:(RCTPromiseRejectBlock)reject {
+  
+  NSString *text = [RCTConvert NSString:options[@"message"]];
+  text = [text stringByAppendingString: [@" " stringByAppendingString: options[@"url"]] ];
+  NSString *whatsAppNumber = [RCTConvert NSString:options[@"whatsAppNumber"]];
+  
+  text = (NSString*)CFBridgingRelease(CFURLCreateStringByAddingPercentEscapes(NULL,(CFStringRef) text, NULL,CFSTR("!*'();:@&=+$,/?%#[]"),kCFStringEncodingUTF8));
+  
+  NSString * urlWhats = whatsAppNumber ? [NSString stringWithFormat:@"whatsapp://send?phone=%@&text=%@", whatsAppNumber, text] : [NSString stringWithFormat:@"whatsapp://send?text=%@", text];
+  NSURL * whatsappURL = [NSURL URLWithString:urlWhats];
+  
+  if ([[UIApplication sharedApplication] canOpenURL: whatsappURL]) {
+      [[UIApplication sharedApplication] openURL: whatsappURL];
+      resolve(@[@true, @""]);
+  }
+}
+
+
+-(MessageType)getMessageType: (NSString *)url {
+  NSArray *imageExtensions = @[@"png", @"jpeg",@"jpg",@"gif"];
+  if([self isMediaType:url mediaExtensions: imageExtensions]){
+    return MessageTypeImage;
+  }
+  
+  NSArray *videoExtensions = @[@".wam", @".mp4"];
+  if([self isMediaType:url mediaExtensions:videoExtensions]){
+    return MessageTypeVideo;
+  }
+  
+  return MessageTypeText;
+}
+
+-(BOOL)isMediaType:(NSString *)url mediaExtensions:(NSArray *)mediaExtensions {
+  for(NSString *extension in mediaExtensions){
+    if([url rangeOfString:extension].location != NSNotFound){
+      return TRUE;
+    }
+  }
+  return FALSE;
+}
+
+-(BOOL)isWhatsAppAvailable {
+  if([[UIApplication sharedApplication] canOpenURL: [NSURL URLWithString:@"whatsapp://app"]]) {
+    NSLog(@"WhastApp installed");
+    return true;
+  }else {
+      // Cannot open whatsapp
+      NSString *appStoreStringURL = @"https://itunes.apple.com/app/whatsapp-messenger/id310633997";
+      NSURL *appStoreURL = [NSURL URLWithString:appStoreStringURL];
+      [[UIApplication sharedApplication] openURL:appStoreURL];
+      return false;
+  }
+}
+
+-(void)handleError:(NSString *)errorMessage code:(NSInteger)code rejectFn:(RCTPromiseRejectBlock)rejectFn {
+  NSDictionary *userInfo = @{NSLocalizedFailureReasonErrorKey: NSLocalizedString(errorMessage, nil)};
+  NSError *error = [NSError errorWithDomain:@"com.rnshare" code:code userInfo:userInfo];
+  
+  NSLog(@"%@", errorMessage);
+  return rejectFn(@"com.rnshare",errorMessage,error);
+}
+
+
+-(BOOL)isAbleToSendMessage:(NSDictionary *) options {
+  if([options objectForKey:@"message"] && [options objectForKey:@"message"] != [NSNull null]){
+    return TRUE;
+  }
+  return FALSE;
+}
+
+-(void)shareMedia:(NSString *)stringURL documentUTI:(NSString *)documentUTI {
+  NSURL *filePath = [NSURL fileURLWithPath:stringURL];
+  documentInteractionController = [UIDocumentInteractionController interactionControllerWithURL:filePath];
+  documentInteractionController.UTI = documentUTI;
+  documentInteractionController.delegate = self;
+  [documentInteractionController presentOpenInMenuFromRect:CGRectZero inView:[[[[[UIApplication sharedApplication] delegate] window] rootViewController] view] animated:YES];
 }
 
 @end
