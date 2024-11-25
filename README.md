7ad699f073fd33c0bb01e94575aaa0d13ba741a8
Anthony LASSERRE <lasserre.anthony@gmail.com>
2024-11-25 21:53:44 +0100

    feat(expo): Expo support with config plugin (#1606)
    * feat: Expo support with config plugin
    
    * fix(ci): wrap-ansi resolutions
    
    BREAKING CHANGE:  adding support to Expo

--- README.md
+++ README.md
@@ -6,6 +6,67 @@ React Native Share, is a simple tool for sharing messages and files with other a
 
 ---
 
+## Expo Managed Workflow
+
+For Expo projects, you will need to be on a managed workflow and use the following command:
+
+```shell
+npx expo install react-native-share
+```
+
+Configure you `app.config.ts` or `app.json` to use the permissions needed by the library:
+
+```json
+{
+  "plugins": [
+    [
+      "react-native-share",
+      {
+        "ios": [
+          "fb",
+          "instagram",
+          "twitter",
+          "tiktoksharesdk",
+        ],
+        "android": [
+          "com.facebook.katana",
+          "com.instagram.android",
+          "com.twitter.android",
+          "com.zhiliaoapp.musically",
+        ]
+      }
+    ]
+  ]
+}
+```
+
+`ios` parameter will take care of adding queries (LSApplicationQueriesSchemes) to the Info.plist.
+
+```xml
+<key>LSApplicationQueriesSchemes</key>
+<array>
+  <string>fb</string>
+  <string>instagram</string>
+  <string>twitter</string>
+  <string>tiktoksharesdk</string>
+</array>
+```
+
+`android` parameter will take care of adding queries to the AndroidManifest.xml.
+
+```xml
+<queries>
+  <package android:name="com.facebook.katana" />
+  <package android:name="com.instagram.android" />
+  <package android:name="com.twitter.android" />
+  <package android:name="com.zhiliaoapp.musically" />
+</queries>
+```
+
+And prebuild the project with `expo prebuild`.
+
+## Bare React Native
+
 If you are using `react-native >= 0.7X` and/or the new arch you just need to do a simple:
 
 ```shell
@@ -21,6 +82,8 @@ npm i react-native-share --save
 After that, we need to install the dependencies to use the project on iOS(you can skip this part if you are using this on Android).
 
 Now run a simple: `npx pod-install` or `cd ios && pod install`. After that, you should be able to use the library on both Platforms, iOS and Android.
+
+# Usage
 
 Then simply import:
 
---
+++ plugin/src/index.ts
@@ -0,0 +1,94 @@
+import {
+  withAndroidManifest,
+  createRunOncePlugin,
+  ExportedConfigWithProps,
+  ExportedConfig,
+} from '@expo/config-plugins';
+
+// eslint-disable-next-line import/no-commonjs, @typescript-eslint/no-var-requires
+const pkg = require('../../package.json');
+
+/**
+ * @type {import('./types').ManifestQueries}
+ * what we are trying to add:
+ * <queries>
+    <package android:name="com.facebook.katana"/>
+    <package android:name="com.instagram.android"/>
+    <package android:name="com.twitter.android"/>
+    <package android:name="com.zhiliaoapp.musically"/>
+    <intent></intent>
+      <action android:name="android.intent.action.VIEW"/>
+      <category android:name="android.intent.category.BROWSABLE"/>
+      <data android:scheme="https"/>
+    </intent>
+  </queries>
+ */
+
+/**
+ * @param {import('@expo/config-plugins').ExportedConfig} config
+ */
+const withAndroidManifestService = (config: ExportedConfig, props: WithSocialShareProps) => {
+  return withAndroidManifest(config, (config: ExportedConfigWithProps) => {
+    config.modResults.manifest = {
+      ...config.modResults.manifest,
+      queries: {
+        package: props?.android?.map((social) => ({
+          $: {
+            'android:name': social,
+          },
+        })),
+        intent: [
+          {
+            action: {
+              $: {
+                'android:name': 'android.intent.action.VIEW',
+              },
+            },
+            category: {
+              $: {
+                'android:name': 'android.intent.category.BROWSABLE',
+              },
+            },
+            data: {
+              $: {
+                'android:scheme': 'https',
+              },
+            },
+          },
+        ],
+      },
+    };
+
+    return config;
+  });
+};
+
+const withInfoPlist = (config: ExportedConfig, props: WithSocialShareProps) => {
+  return {
+    ...config,
+    ios: {
+      ...config.ios,
+      infoPlist: {
+        ...config.ios?.infoPlist,
+        LSApplicationQueriesSchemes: {
+          ...config.ios?.infoPlist?.LSApplicationQueriesSchemes,
+          ...props?.ios,
+        },
+      },
+    },
+  };
+};
+
+type WithSocialShareProps = {
+  ios: string[];
+  android: string[];
+};
+
+function withSocialShare(config: ExportedConfig, props: WithSocialShareProps) {
+  config = withAndroidManifestService(config, props); // Android
+  config = withInfoPlist(config, props); // iOS
+  return config;
+}
+
+// eslint-disable-next-line import/no-commonjs
+module.exports = createRunOncePlugin(withSocialShare, pkg.name, pkg.version);
