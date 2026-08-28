b50d20478df17ac9dfb6a31a63e54aefa972f677
Oskar Eichler <62393985+OskarEichler@users.noreply.github.com>
2026-08-28 14:53:08 +0300

    fix(ios): expose extension MIME lookup

--- ios/RNShareUtils.h
+++ ios/RNShareUtils.h
@@ -3,6 +3,7 @@
 @interface RNShareUtils : NSObject
 +(NSString*)getExtensionFromBase64:(NSString*)base64String;
 +(NSString*)getMimeTypeFromBase64:(NSString*)base64String;
++(NSString*)getMimeTypeFromExtension:(NSString*)extension;
 +(NSString*)getTypeIdentifierFromExtension:(NSString*)extension;
 +(NSURL*)getPathFromBase64:(NSString*)base64String with:(NSData*)data fileName:(NSString*)name;
 +(NSURL*)getPathFromFilename:(NSString*)filename with:(NSData*)data;
