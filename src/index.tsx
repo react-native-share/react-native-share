ec3a858fc72b08db156a52e5cc8f473f69529dde
Oskar Eichler <62393985+OskarEichler@users.noreply.github.com>
2026-08-28 09:10:41 +0300

    fix: reduce bridge work and preserve public share contracts

--- src/index.tsx
+++ src/index.tsx
@@ -8,7 +8,6 @@ import checkPermissions from './helpers/checkPermissions';
 import {
   Social,
   IsPackageInstalledResult,
-  ActivityType,
   ShareAsset,
   ShareOpenResult,
   ShareOptions,
@@ -27,24 +26,27 @@ const RNShare = {
   Sheet,
 
   Social: {
-    FACEBOOK: constants.FACEBOOK || Social.Facebook,
-    FACEBOOK_STORIES: constants.FACEBOOKSTORIES || Social.FacebookStories,
-    PAGESMANAGER: constants.PAGESMANAGER || Social.Pagesmanager,
-    TWITTER: constants.TWITTER || Social.Twitter,
-    WHATSAPP: constants.WHATSAPP || Social.Whatsapp,
-    WHATSAPPBUSINESS: constants.WHATSAPPBUSINESS || Social.Whatsappbusiness,
-    INSTAGRAM: constants.INSTAGRAM || Social.Instagram,
-    INSTAGRAM_STORIES: constants.INSTAGRAMSTORIES || Social.InstagramStories,
-    GOOGLEPLUS: constants.GOOGLEPLUS || Social.Googleplus,
-    EMAIL: constants.EMAIL || Social.Email,
-    PINTEREST: constants.PINTEREST || Social.Pinterest,
-    LINKEDIN: constants.LINKEDIN || Social.Linkedin,
-    SMS: constants.SMS || Social.Sms,
-    TELEGRAM: constants.TELEGRAM || Social.Telegram,
-    MESSENGER: constants.MESSENGER || Social.Messenger,
-    SNAPCHAT: constants.SNAPCHAT || Social.Snapchat,
-    VIBER: constants.VIBER || Social.Viber,
-    DISCORD: constants.DISCORD || Social.Discord,
+    FACEBOOK: (constants.FACEBOOK || Social.Facebook) as Social.Facebook,
+    FACEBOOK_STORIES: (constants.FACEBOOKSTORIES ||
+      Social.FacebookStories) as Social.FacebookStories,
+    PAGESMANAGER: (constants.PAGESMANAGER || Social.Pagesmanager) as Social.Pagesmanager,
+    TWITTER: (constants.TWITTER || Social.Twitter) as Social.Twitter,
+    WHATSAPP: (constants.WHATSAPP || Social.Whatsapp) as Social.Whatsapp,
+    WHATSAPPBUSINESS: (constants.WHATSAPPBUSINESS ||
+      Social.Whatsappbusiness) as Social.Whatsappbusiness,
+    INSTAGRAM: (constants.INSTAGRAM || Social.Instagram) as Social.Instagram,
+    INSTAGRAM_STORIES: (constants.INSTAGRAMSTORIES ||
+      Social.InstagramStories) as Social.InstagramStories,
+    GOOGLEPLUS: (constants.GOOGLEPLUS || Social.Googleplus) as Social.Googleplus,
+    EMAIL: (constants.EMAIL || Social.Email) as Social.Email,
+    PINTEREST: (constants.PINTEREST || Social.Pinterest) as Social.Pinterest,
+    LINKEDIN: (constants.LINKEDIN || Social.Linkedin) as Social.Linkedin,
+    SMS: (constants.SMS || Social.Sms) as Social.Sms,
+    TELEGRAM: (constants.TELEGRAM || Social.Telegram) as Social.Telegram,
+    MESSENGER: (constants.MESSENGER || Social.Messenger) as Social.Messenger,
+    SNAPCHAT: (constants.SNAPCHAT || Social.Snapchat) as Social.Snapchat,
+    VIBER: (constants.VIBER || Social.Viber) as Social.Viber,
+    DISCORD: (constants.DISCORD || Social.Discord) as Social.Discord,
   },
 
   async open(options: ShareOptions) {
@@ -76,6 +78,9 @@ const RNShare = {
     if (options.social === RNShare.Social.INSTAGRAM_STORIES && !options.appId) {
       throw new Error('To share to Instagram Stories you need to provide appId');
     }
+    if (options.social === RNShare.Social.FACEBOOK_STORIES && !options.appId) {
+      throw new Error('To share to Facebook Stories you need to provide appId');
+    }
 
     await checkPermissions(options);
 
@@ -98,7 +103,7 @@ const RNShare = {
 
     const result: IsPackageInstalledResult = {
       isInstalled,
-      message: 'Package is Installed',
+      message: isInstalled ? 'Package is Installed' : 'Package is not Installed',
     };
 
     return result;
@@ -106,7 +111,17 @@ const RNShare = {
 } as const;
 
 export { Overlay, Sheet, Button, ShareSheet, ShareAsset, Social };
-export type { ShareSingleOptions, ShareOptions, ActivityType, IsPackageInstalledResult };
+export type {
+  ShareSingleOptions,
+  ShareOptions,
+  ActivityType,
+  ActivityItem,
+  ActivityItemSource,
+  LinkMetadata,
+  ShareOpenResult,
+  ShareSingleResult,
+  IsPackageInstalledResult,
+} from './types';
 export type { OverlayProps } from './components/Overlay';
 export type { SheetProps } from './components/Sheet';
 export type { ButtonProps } from './components/Button';
